411aeca83f23f5bd809bcf74f3e8e78de1ad8741
s7138 <s7138@CR457975.europe.ifint.biz>
2018-05-28 09:59:54 +0300

    initial commit

---
+++ src/main/java/Pyry/Testi1/main.java
@@ -0,0 +1,29 @@
+package Pyry.Testi1;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(urlPatterns="testi")
+public class main extends HttpServlet{
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    response.getWriter().append("Testaillaan").append(request.getContextPath());
+	    response.getWriter().append("1+1=" + (1+1));
+	}
+
+	
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    doGet(request, response);
+	}
+
+
+
+	
+}
